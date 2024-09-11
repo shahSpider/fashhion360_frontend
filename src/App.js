1171519cb149cb9613e27e0080e78b591e75154d
shahspider@github.com <aq.syed007@gmail.com>
2024-09-12 02:03:05 +0500

    add login, logout and register basic functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,188 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './components/Login';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+  baseURL: "http://127.0.0.1:8000"
+});
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(false);
+  const [registrationToggle, setRegistrationToggle] = useState(false);
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const isAuthenticated = localStorage.getItem('authenticated') === 'true';
+  
+    if (isAuthenticated) {
+      // Verify with the backend
+      client.get("/api/user/")
+        .then(function(res) {
+          setCurrentUser(true);
+        })
+        .catch(function(error) {
+          setCurrentUser(false);
+          localStorage.removeItem('authenticated');
+        });
+    } else {
+      setCurrentUser(false);
+    }
+  }, []);
+  
+
+  function update_form_btn() {
+    if (registrationToggle) {
+      document.getElementById("form_btn").innerHTML = "Register";
+      setRegistrationToggle(false);
+    } else {
+      document.getElementById("form_btn").innerHTML = "Log in";
+      setRegistrationToggle(true);
+    }
+  }
+
+  function submitRegistration(e) {
+    e.preventDefault();
+    client.post(
+      "/api/register/",
+      {
+        email: email,
+        username: username,
+        password: password
+      }
+    ).then(function(res) {
+      client.post(
+        "/api/login/",
+        {
+          username: username,
+          password: password
+        }
+      ).then(function(res) {
+        // Save authentication state to local storage
+    localStorage.setItem('authenticated', 'true');
+        setCurrentUser(true);
+      });
+    });
+  }
+
+  function submitLogin(e) {
+    e.preventDefault();
+    client.post(
+      "/api/login/",
+      {
+        username: username,
+        password: password
+      }
+    ).then(function(res) {
+      // Save authentication state to local storage
+    localStorage.setItem('authenticated', 'true');
+      setCurrentUser(true);
+    });
+  }
+
+  function submitLogout(e) {
+    e.preventDefault();
+    client.post(
+      "/api/logout/",
+    ).then(function(res) {
+      localStorage.removeItem('authenticated');
+      setCurrentUser(false);
+    });
+  }
+
+  console.info("current User", currentUser)
+  if (currentUser) {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>Authentication App</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <form onSubmit={e => submitLogout(e)}>
+                  <Button type="submit" variant="light">Log out</Button>
+                </form>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+          <div className="center">
+            <h2>You're logged in!</h2>
+          </div>
+        </div>
+    );
+  }
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>Authentication App</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <Button id="form_btn" onClick={update_form_btn} variant="light">Register</Button>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    {
+      registrationToggle ? (
+        <div className="center">
+          <Form onSubmit={e => submitRegistration(e)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>        
+      ) : (
+        <div className="center">
+          <Form onSubmit={e => submitLogin(e)}>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Enter Username" value={username} onChange={e => setUsername(e.target.value)} />
+              <Form.Text className="text-muted">
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      )
+    }
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/esm/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavbarSection() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+		 <Navbar.Text>
+			<Button id="form_btn" onClick="" variant="dark">Register</Button>
+		 </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarSection;
